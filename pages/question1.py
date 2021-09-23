03bdd1352c42e3566143e4629bb975a96c70353d
HY <hanyikoh@gmail.com>
2021-09-23 20:17:58 +0800

    feat(q1):attach tables + outliers for all dataset

--- pages/question1.py
+++ pages/question1.py
@@ -30,7 +30,7 @@ def app():
         state_case_df = state_case_df.loc[between_two_dates]
         st.write('First 5 rows of the dataset')
         st.table(state_case_df.head())
-
+        st.write('Outliers detection with Boxplot')
         fig, axes = plt.subplots(1, 3, figsize=(15, 5), sharey=True)
         # fig.suptitle('Outliers Visualization')
         plt.subplots_adjust(left=None, bottom= 0.1, right=None, top=1, wspace=0.2, hspace=0.6)
@@ -43,6 +43,7 @@ def app():
         axes[2].set_title('Recovered Case')
         st.set_option('deprecation.showPyplotGlobalUse', False)
         st.pyplot()
+
     elif chosen == "Clusters":
         clusters_df = pd.read_csv(clusters_dir)
         after_start_date = clusters_df["date_announced"] >= start_date
@@ -50,6 +51,9 @@ def app():
         between_two_dates = after_start_date & before_end_date
         clusters_df = clusters_df.loc[between_two_dates]
         clusters_df['date'] = clusters_df.date_announced
+        st.write('First 5 rows of the dataset')
+        st.table(clusters_df.head())
+        st.write('Outliers detection with Boxplot')
         fig, axes = plt.subplots(3, 3, figsize=(15, 5), sharey=True)
         # fig.suptitle('Outliers Visualization')
         plt.subplots_adjust(left=None, bottom= 0.1, right=None, top=2, wspace=0.2, hspace=0.6)
@@ -77,7 +81,9 @@ def app():
         before_end_date = states_tests_df["date"] <= end_date
         between_two_dates = after_start_date & before_end_date
         states_tests_df = states_tests_df.loc[between_two_dates]
-
+        st.write('First 5 rows of the dataset')
+        st.table(states_tests_df.head())
+        st.write('Outliers detection with Boxplot')
         fig, axes = plt.subplots(1, 3, figsize=(15, 5), sharey=True)
         # fig.suptitle('Outliers Visualization')
         plt.subplots_adjust(left=None, bottom= 0.1, right=None, top=0.5, wspace=0.2, hspace=0.6)
@@ -96,7 +102,9 @@ def app():
         before_end_date = malaysia_case_df["date"] <= end_date
         between_two_dates = after_start_date & before_end_date
         malaysia_case_df = malaysia_case_df.loc[between_two_dates]
-
+        st.write('First 5 rows of the dataset')
+        st.table(malaysia_case_df.head())
+        st.write('Outliers detection with Boxplot')
         fig, axes = plt.subplots(4, 3, figsize=(15, 5), sharey=True)
         # fig.suptitle('Outliers Visualization')
         plt.subplots_adjust(left=None, bottom= 0.1, right=None, top=2, wspace=0.2, hspace=0.6)
@@ -124,19 +132,99 @@ def app():
         st.set_option('deprecation.showPyplotGlobalUse', False)
         st.pyplot()
 
-    elif chosen == "Malaysia Tests":
-        malaysia_tests_df = pd.read_csv(malaysia_tests_dir)
-        after_start_date = malaysia_tests_df["date"] >= start_date
-        before_end_date = malaysia_tests_df["date"] <= end_date
+    elif chosen == "PKRC":
+        pkrc_df = pd.read_csv(pkrc_dir)
+        after_start_date = pkrc_df["date"] >= start_date
+        before_end_date = pkrc_df["date"] <= end_date
         between_two_dates = after_start_date & before_end_date
-        malaysia_tests_df = malaysia_tests_df.loc[between_two_dates]
+        pkrc_df = pkrc_df.loc[between_two_dates]
+        st.write('First 5 rows of the dataset')
+        st.table(pkrc_df.head())
+        st.write('Outliers detection with Boxplot')
+        fig, axes = plt.subplots(4, 3, figsize=(15, 5), sharey=True)
+        # fig.suptitle('Outliers Visualization')
+        plt.subplots_adjust(left=None, bottom= 0.1, right=None, top=2, wspace=0.2, hspace=0.6)
 
+        sns.boxplot(data=pkrc_df, x = pkrc_df["beds"],ax=axes[0][0])
+        axes[0][0].set_title('beds')
+        sns.boxplot(data=pkrc_df,x = pkrc_df["admitted_pui"],ax=axes[0][1])
+        axes[0][1].set_title('admitted_pui')
+        sns.boxplot(data=pkrc_df, x = pkrc_df["admitted_covid"],ax=axes[0][2])
+        axes[0][2].set_title("admitted_covid")
+        sns.boxplot(data=pkrc_df,x = pkrc_df["admitted_total"],ax=axes[1][0])
+        axes[1][0].set_title('admitted_total')
+        sns.boxplot(data=pkrc_df, x = pkrc_df["discharge_pui"],ax=axes[1][1])
+        axes[1][1].set_title('discharge_pui')
+        sns.boxplot(data=pkrc_df,x = pkrc_df["discharge_covid"],ax=axes[1][2])
+        axes[1][2].set_title('discharge_covid')
+        sns.boxplot(data=pkrc_df, x = pkrc_df["discharge_total"],ax=axes[2][0])
+        axes[2][0].set_title('discharge_total')
+        sns.boxplot(data=pkrc_df,x = pkrc_df["pkrc_covid"],ax=axes[2][1])
+        axes[2][1].set_title('pkrc_covid')
+        sns.boxplot(data=pkrc_df, x = pkrc_df["pkrc_pui"],ax=axes[2][2])
+        axes[2][2].set_title('pkrc_pui')
+        sns.boxplot(data=pkrc_df,x = pkrc_df["pkrc_noncovid"],ax=axes[3][0])
+        axes[2][0].set_title('pkrc_noncovid')
+
+        st.set_option('deprecation.showPyplotGlobalUse', False)
+        st.pyplot()
+
+    elif chosen == "State CheckIn":
+        checkIn_df = pd.read_csv(checkIn_dir)
+        after_start_date = checkIn_df["date"] >= start_date
+        before_end_date = checkIn_df["date"] <= end_date
+        between_two_dates = after_start_date & before_end_date
+        checkIn_df = checkIn_df.loc[between_two_dates]
+        checkIn_df.head()
+        st.write('First 5 rows of the dataset')
+        st.table(checkIn_df.head())
+        st.write('Outliers detection with Boxplot')
         fig, axes = plt.subplots(1, 3, figsize=(15, 5), sharey=True)
+        # fig.suptitle('Outliers Visualization')
         plt.subplots_adjust(left=None, bottom= 0.1, right=None, top=0.5, wspace=0.2, hspace=0.6)
-        sns.boxplot(data=malaysia_tests_df, x = malaysia_tests_df["rtk-ag"],ax=axes[0])
-        axes[0].set_title('rtk-ag')
-        sns.boxplot(data=malaysia_tests_df,x = malaysia_tests_df["pcr"],ax=axes[1])
-        axes[1].set_title('pcr')
 
+        sns.boxplot(data=checkIn_df, x = checkIn_df["checkins"],ax=axes[0])
+        axes[0].set_title('checkins')
+        sns.boxplot(data=checkIn_df,x = checkIn_df["unique_ind"],ax=axes[1])
+        axes[1].set_title('unique_ind')
+        sns.boxplot(data=checkIn_df, x = checkIn_df["unique_loc"],ax=axes[2])
+        axes[1].set_title('unique_loc')
+
+        st.set_option('deprecation.showPyplotGlobalUse', False)
+        st.pyplot()
+
+    elif chosen == "PKRC":
+        pkrc_df = pd.read_csv(pkrc_dir)
+        after_start_date = pkrc_df["date"] >= start_date
+        before_end_date = pkrc_df["date"] <= end_date
+        between_two_dates = after_start_date & before_end_date
+        pkrc_df = pkrc_df.loc[between_two_dates]
+        st.write('First 5 rows of the dataset')
+        st.table(pkrc_df.head())
+        st.write('Outliers detection with Boxplot')
+        fig, axes = plt.subplots(4, 3, figsize=(15, 5), sharey=True)
+        # fig.suptitle('Outliers Visualization')
+        plt.subplots_adjust(left=None, bottom= 0.1, right=None, top=2, wspace=0.2, hspace=0.6)
+
+        sns.boxplot(data=pkrc_df, x = pkrc_df["beds"],ax=axes[0][0])
+        axes[0][0].set_title('beds')
+        sns.boxplot(data=pkrc_df,x = pkrc_df["admitted_pui"],ax=axes[0][1])
+        axes[0][1].set_title('admitted_pui')
+        sns.boxplot(data=pkrc_df, x = pkrc_df["admitted_covid"],ax=axes[0][2])
+        axes[0][2].set_title("admitted_covid")
+        sns.boxplot(data=pkrc_df,x = pkrc_df["admitted_total"],ax=axes[1][0])
+        axes[1][0].set_title('admitted_total')
+        sns.boxplot(data=pkrc_df, x = pkrc_df["discharge_pui"],ax=axes[1][1])
+        axes[1][1].set_title('discharge_pui')
+        sns.boxplot(data=pkrc_df,x = pkrc_df["discharge_covid"],ax=axes[1][2])
+        axes[1][2].set_title('discharge_covid')
+        sns.boxplot(data=pkrc_df, x = pkrc_df["discharge_total"],ax=axes[2][0])
+        axes[2][0].set_title('discharge_total')
+        sns.boxplot(data=pkrc_df,x = pkrc_df["pkrc_covid"],ax=axes[2][1])
+        axes[2][1].set_title('pkrc_covid')
+        sns.boxplot(data=pkrc_df, x = pkrc_df["pkrc_pui"],ax=axes[2][2])
+        axes[2][2].set_title('pkrc_pui')
+        sns.boxplot(data=pkrc_df,x = pkrc_df["pkrc_noncovid"],ax=axes[3][0])
+        axes[2][0].set_title('pkrc_noncovid')
         st.set_option('deprecation.showPyplotGlobalUse', False)
         st.pyplot()
